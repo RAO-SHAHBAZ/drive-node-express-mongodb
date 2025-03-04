fd4f8077ac421f049867b1ec0bc0d4aa6f5cc2e7
Rao SHahbaz <menlenon59@gmail.com>
2025-03-04 07:15:21 +0500

    Drive App

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const userRouter = require('./routes/user.routes')
+
+app.set("view engine" , "ejs")
+
+app.use('/user',userRouter)
+app.listen(3000,()=>{
+    console.log(
+        "Server is Runing on Port 3000"
+    ); 
+})
